197394bd41c4f5819bdc82cf810b36b4b59b0595
Krish <krishpatel.1097@gmail.com>
2025-07-28 18:37:20 -0400

    added server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'docs', 'about.html')));
+app.get('/education', (req, res) => res.sendFile(path.join(__dirname, 'docs', 'education.html')));
+app.get('/experience', (req, res) => res.sendFile(path.join(__dirname, 'docs', 'experience.html')));
+app.get('/projects', (req, res) => res.sendFile(path.join(__dirname, 'docs', 'projects.html')));
+app.get('/skills', (req, res) => res.sendFile(path.join(__dirname, 'docs', 'skills.html')));
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
