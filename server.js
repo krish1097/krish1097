8ae5e4842efd1bcf4ce40b0397b11d07c9815f3d
Krish <krishpatel.1097@gmail.com>
2025-08-14 16:09:44 -0400

    completed 25% of aboutme page and changed src folder name to docs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'docs', 'index.html')));
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'views', 'about.html')));
 app.get('/projects', (req, res) => res.sendFile(path.join(__dirname, 'views', 'projects.html')));
 app.get('/experience', (req, res) => res.sendFile(path.join(__dirname, 'views', 'experience.html')));
